fce69ee7f3d0d7db2348b96ed522a53c998612e9
Manasvi Sharma <114683045+manasvi2821@users.noreply.github.com>
2025-09-24 00:12:49 +0530

    region updated

--- strapi-terraform/variables.tf
+++ strapi-terraform/variables.tf
@@ -1,7 +1,7 @@
 variable "aws_region" {
   description = "AWS region"
   type        = string
-  default     = "us-east-1"
+  default     = "ap-south-1"
 }
 
 variable "instance_type" {
@@ -18,5 +18,5 @@ variable "key_name" {
 variable "ami_id" {
   description = "AMI ID for EC2"
   type        = string
-  default     = "ami-0c02fb55956c7d316" # Amazon Linux 2 in us-east-1
+  default     = "ami-0c02fb55956c7d316" 
 }
