42c789998f72b65cc5b15b17609b3c10d06925f7
Manasvi Sharma <114683045+manasvi2821@users.noreply.github.com>
2025-09-24 14:25:46 +0530

    updated terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -10,27 +10,57 @@ resource "aws_security_group" "ec2_sg" {
   vpc_id      = data.aws_vpc.default.id
 
   ingress {
-    description      = "SSH"
-    from_port        = 22
-    to_port          = 22
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress {
-    description      = "HTTP"
-    from_port        = 80
-    to_port          = 80
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
+}
+
+# IAM Role for EC2 with ECR Access
+resource "aws_iam_role" "ec2_ecr_full_access_role" {
+  name = "ec2_ecr_full_access_role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        },
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+# Attach AWS Managed Policy (ECR Full Access)
+resource "aws_iam_role_policy_attachment" "ec2_ecr_full_access_attach" {
+  role       = aws_iam_role.ec2_ecr_full_access_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryFullAccess"
+}
+
+# Instance Profile for EC2
+resource "aws_iam_instance_profile" "ec2_ecr_profile" {
+  name = "ec2_ecr_profile"
+  role = aws_iam_role.ec2_ecr_full_access_role.name
 }
 
 # EC2 Instance
@@ -39,6 +69,8 @@ resource "aws_instance" "docker_ec2" {
   instance_type          = var.instance_type
   key_name               = var.key_name
   vpc_security_group_ids = [aws_security_group.ec2_sg.id]
+
+  iam_instance_profile   = aws_iam_instance_profile.ec2_ecr_profile.name
 
   tags = {
     Name = "Manasvi-strapi-server"
