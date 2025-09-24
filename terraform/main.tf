979a7121efe73576ca82267dd1dd8cfff350e280
Manasvi Sharma <114683045+manasvi2821@users.noreply.github.com>
2025-09-24 17:26:49 +0530

    updated terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -33,44 +33,56 @@ resource "aws_security_group" "ec2_sg" {
   }
 }
 
-# IAM Role for EC2 with ECR Access
+############################
+# IAM Role & Instance Profile
+############################
+
+# Create IAM Role for EC2
 resource "aws_iam_role" "ec2_ecr_full_access_role" {
   name = "ec2_ecr_full_access_role"
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17",
-    Statement = [
-      {
-        Effect = "Allow",
-        Principal = {
-          Service = "ec2.amazonaws.com"
-        },
-        Action = "sts:AssumeRole"
-      }
-    ]
+    Statement = [{
+      Effect = "Allow",
+      Principal = { Service = "ec2.amazonaws.com" },
+      Action   = "sts:AssumeRole"
+    }]
   })
 }
 
-# Attach AWS Managed Policy (ECR Full Access)
-resource "aws_iam_role_policy_attachment" "ec2_ecr_full_access_attach" {
+# Attach AWS Managed Policies
+resource "aws_iam_role_policy_attachment" "ecr_full" {
   role       = aws_iam_role.ec2_ecr_full_access_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryFullAccess"
 }
 
-# Instance Profile for EC2
-resource "aws_iam_instance_profile" "ec2_ecr_profile" {
-  name = "ec2_ecr_profile"
+resource "aws_iam_role_policy_attachment" "ec2_full" {
+  role       = aws_iam_role.ec2_ecr_full_access_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2FullAccess"
+}
+
+resource "aws_iam_role_policy_attachment" "ssm_core" {
+  role       = aws_iam_role.ec2_ecr_full_access_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+}
+
+# Create Instance Profile for the Role
+resource "aws_iam_instance_profile" "ec2_ecr_full_access_profile" {
+  name = "ec2_ecr_full_access_profile"
   role = aws_iam_role.ec2_ecr_full_access_role.name
 }
 
+############################
 # EC2 Instance
+############################
 resource "aws_instance" "docker_ec2" {
   ami                    = var.ami_id
   instance_type          = var.instance_type
   key_name               = var.key_name
   vpc_security_group_ids = [aws_security_group.ec2_sg.id]
 
-  iam_instance_profile   = aws_iam_instance_profile.ec2_ecr_profile.name
+  iam_instance_profile   = aws_iam_instance_profile.ec2_ecr_full_access_profile.name
 
   tags = {
     Name = "Manasvi-strapi-server"
