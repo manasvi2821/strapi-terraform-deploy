87791b09a508cf49df09c7a1b7077bd2ddffffad
Manasvi Sharma <114683045+manasvi2821@users.noreply.github.com>
2025-09-28 23:47:12 +0530

    Add ECS cluster and service outputs for GitHub workflow

--- terraform02/outputs.tf
+++ terraform02/outputs.tf
@@ -1,3 +1,11 @@
 output "alb_dns_name" {
   value = aws_lb.alb.dns_name
 }
+
+output "ecs_cluster_name" {
+  value = aws_ecs_cluster.cluster.name
+}
+
+output "ecs_service_name" {
+  value = aws_ecs_service.service.name
+}
